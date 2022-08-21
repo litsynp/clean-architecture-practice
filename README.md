e71fc5ad2d5e43d76404adf77070c8ae8475628d
litsynp <nocte55is@gmail.com>
2022-08-21 22:53:59 +0900

    build: init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Clean Architecture Practice
+
+This repository is made to play with hexagonal architecture with Spring Boot.
---
+++ src/main/java/com/litsynp/cleanarchitecture/Application.java
@@ -0,0 +1,13 @@
+package com.litsynp.cleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
