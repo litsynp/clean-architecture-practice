64cd7e0a6cc9b73979cb7260c54a6ac2d97df071
litsynp <nocte55is@gmail.com>
2022-08-23 02:44:25 +0900

    test: add test

---
+++ src/test/java/com/litsynp/cleanarchitecture/account/domain/AccountTest.java
@@ -0,0 +1,98 @@
+package com.litsynp.cleanarchitecture.account.domain;
+
+import com.litsynp.cleanarchitecture.account.domain.Account.AccountId;
+import org.junit.jupiter.api.Test;
+
+import static com.litsynp.cleanarchitecture.common.AccountTestData.defaultAccount;
+import static com.litsynp.cleanarchitecture.common.ActivityTestData.defaultActivity;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AccountTest {
+
+    @Test
+    void calculatesBalance() {
+        AccountId accountId = new AccountId(1L);
+        Account account = defaultAccount()
+                .withAccountId(accountId)
+                .withBaselineBalance(Money.of(555L))
+                .withActivityWindow(new ActivityWindow(
+                        defaultActivity()
+                                .withTargetAccount(accountId)
+                                .withMoney(Money.of(999L)).build(),
+                        defaultActivity()
+                                .withTargetAccount(accountId)
+                                .withMoney(Money.of(1L)).build()))
+                .build();
+
+        Money balance = account.calculateBalance();
+
+        assertThat(balance).isEqualTo(Money.of(1555L));
+    }
+
+    @Test
+    void withdrawalSucceeds() {
+        AccountId accountId = new AccountId(1L);
+        Account account = defaultAccount()
+                .withAccountId(accountId)
+                .withBaselineBalance(Money.of(555L))
+                .withActivityWindow(new ActivityWindow(
+                        defaultActivity()
+                                .withTargetAccount(accountId)
+                                .withMoney(Money.of(999L)).build(),
+                        defaultActivity()
+                                .withTargetAccount(accountId)
+                                .withMoney(Money.of(1L)).build()))
+                .build();
+
+        boolean success = account.withdraw(Money.of(555L), new AccountId(99L));
+
+        assertThat(success).isTrue();
+        assertThat(account.getActivityWindow().getActivities()).hasSize(3);
+        assertThat(account.calculateBalance()).isEqualTo(Money.of(1000L));
+    }
+
+    @Test
+    void withdrawalFailure() {
+        AccountId accountId = new AccountId(1L);
+        Account account = defaultAccount()
+                .withAccountId(accountId)
+                .withBaselineBalance(Money.of(555L))
+                .withActivityWindow(new ActivityWindow(
+                        defaultActivity()
+                                .withTargetAccount(accountId)
+                                .withMoney(Money.of(999L)).build(),
+                        defaultActivity()
+                                .withTargetAccount(accountId)
+                                .withMoney(Money.of(1L)).build()))
+                .build();
+
+        boolean success = account.withdraw(Money.of(1556L), new AccountId(99L));
+
+        assertThat(success).isFalse();
+        assertThat(account.getActivityWindow().getActivities()).hasSize(2);
+        assertThat(account.calculateBalance()).isEqualTo(Money.of(1555L));
+    }
+
+    @Test
+    void depositSuccess() {
+        AccountId accountId = new AccountId(1L);
+        Account account = defaultAccount()
+                .withAccountId(accountId)
+                .withBaselineBalance(Money.of(555L))
+                .withActivityWindow(new ActivityWindow(
+                        defaultActivity()
+                                .withTargetAccount(accountId)
+                                .withMoney(Money.of(999L)).build(),
+                        defaultActivity()
+                                .withTargetAccount(accountId)
+                                .withMoney(Money.of(1L)).build()))
+                .build();
+
+        boolean success = account.deposit(Money.of(445L), new AccountId(99L));
+
+        assertThat(success).isTrue();
+        assertThat(account.getActivityWindow().getActivities()).hasSize(3);
+        assertThat(account.calculateBalance()).isEqualTo(Money.of(2000L));
+    }
+
+}
