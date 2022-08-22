257fa58508a5d02a6120fe0ca627b91c1148eb49
litsynp <nocte55is@gmail.com>
2022-08-23 02:44:08 +0900

    fix: fix to use @Value for test

--- src/main/java/com/litsynp/cleanarchitecture/account/application/port/in/SendMoneyCommand.java
+++ src/main/java/com/litsynp/cleanarchitecture/account/application/port/in/SendMoneyCommand.java
@@ -3,11 +3,13 @@ package com.litsynp.cleanarchitecture.account.application.port.in;
 import com.litsynp.cleanarchitecture.account.domain.Account.AccountId;
 import com.litsynp.cleanarchitecture.account.domain.Money;
 import com.litsynp.cleanarchitecture.common.SelfValidating;
-import lombok.Getter;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
 
 import javax.validation.constraints.NotNull;
 
-@Getter
+@Value
+@EqualsAndHashCode(callSuper = false)
 public class SendMoneyCommand extends SelfValidating<SendMoneyCommand> {
 
     @NotNull
